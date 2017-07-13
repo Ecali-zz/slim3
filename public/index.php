916d0a9a160199f83d8e5b8e6dc571cb49ab3aac
Ecali301 <ecali301@gmail.xom>
2017-07-13 14:11:17 +0200

    add index file

--- public/index.php
+++ public/index.php
@@ -1,18 +1,19 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Ecali
+ * Date: 13/07/17
+ * Time: 14:10
+ */
 
 require '../vendor/autoload.php';
-
 session_start();
-
-
 $app= new \Slim\App([
     'settings' => [
         'displayErrorDetails' => true
     ]
 ]);
-
 require '../app/container.php';
-
 $app->get('/',\App\Controllers\PageController::class . ':home');
 $app->get('/test',\App\Controllers\PageController::class . ':test');
 $app->get('/login',\App\Controllers\PageController::class . ':login');
@@ -23,6 +24,4 @@ $app->get('/user/{user}',\App\Controllers\PageController::class . ':user');
 $app->get('/add/video',\App\Controllers\PageController::class . ':addvideo')->setName('addvideo');
 $app->any('/view/video',\App\Controllers\PageController::class . ':viewvideo')->setName('protect');
 $app->any('/view/video/',\App\Controllers\PageController::class . ':singlevideo');
-
-
 $app->run();
